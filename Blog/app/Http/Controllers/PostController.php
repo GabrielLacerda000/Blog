3272cfb95ffa9c9ec7b4a31d5c2a3a10d8c64e77
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2024-07-27 12:04:00 -0300

    added policies

--- Blog/app/Http/Controllers/PostController.php
+++ Blog/app/Http/Controllers/PostController.php
@@ -6,24 +6,20 @@ use App\Http\Requests\PostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Support\Facades\Auth;
+use App\Traits\Authorization;
 
 class PostController extends Controller {
+    use Authorization;
 
     public function index() {
-        // return response()->json(Post::with('user')->get());
+        
         return PostResource::collection(Post::all());
     }
     
     public function store(PostRequest $request) {
         
-        $post = Post::create([
-            'title' => $request->title,
-            'content' => $request->content,
-            'tag_id' => $request->tag_id,
-            'user_id' => auth()->user()->id,
-        ]);
-
+        $post = auth()->user()->post()->create($request->validated());
+        
         return new PostResource($post);
     }
 
@@ -34,16 +30,14 @@ class PostController extends Controller {
     
     public function update(UpdatePostRequest $request, Post $post) {
 
-        if(auth()->user()->id !== $post->user_id) {
+        if($request->user()->cannot('update', $post)) {
             return response()->json(['error' => 'Unauthorized'], 403);
         }
-        
-        $post->update([
-            'title' => $request->title,
-            'content' => $request->content,
-            'tag_id' => $request->tag_id,
-            'user_id' => auth()->user()->id,
-        ]);
+
+        $data = $request->validated();
+        $data['user_id'] = auth()->user()->id;
+
+        $post->update($data);
 
         return new PostResource($post);
     }
@@ -51,9 +45,10 @@ class PostController extends Controller {
     public function destroy(Post $post) {
 
         if(auth()->user()->id !== $post->user_id) {
+        
             return response()->json(['error' => 'Unauthorized'], 403);
         }
-
+      
         $post->delete();
         return response()->json(['message' => 'Post deleted successfully'], 200);
     }
